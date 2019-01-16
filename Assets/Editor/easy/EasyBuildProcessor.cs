dbdca3e25cc5a83e45322d45831035ccedb39f7b
zhangfenglin <zhangfenglin@apusapps.com>
2019-01-16 13:16:34 +0800

    自动导入动态库到embed中

---
+++ Assets/Editor/easy/EasyBuildProcessor.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.iOS.Xcode;
+using UnityEditor.iOS.Xcode.Extensions;
+using System.IO;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using easy;
+
+public class EasyBuildProcessor : IPostprocessBuildWithReport
+{
+    private static readonly string FRAMEWORKS_BASE_PATH = "Plugins/iOS";
+
+    public int callbackOrder { get { return 0; } }
+
+    public void OnPostprocessBuild(BuildReport report)
+    {
+        if (report.summary.platform == BuildTarget.iOS)
+        {
+#if UNITY_EDITOR_OSX
+            string projectPath = PBXProject.GetPBXProjectPath(report.summary.outputPath);
+            PBXProject proj = new PBXProject();
+            proj.ReadFromString(File.ReadAllText(projectPath));
+            string targetId = proj.TargetGuidByName("Unity-iPhone");
+            AddDynamicFrameworkToEmbed(report.summary.outputPath, proj, targetId, new DirectoryInfo(Path.Combine(Application.dataPath, FRAMEWORKS_BASE_PATH)).GetDirectories());
+            proj.SetBuildProperty(targetId, "LD_RUNPATH_SEARCH_PATHS", "$(inherited) @executable_path/Frameworks");
+            proj.WriteToFile(projectPath);
+#endif
+        }
+    }
+
+#if UNITY_EDITOR_OSX
+    private void AddDynamicFrameworkToEmbed(string pathToBuiltProject, PBXProject proj, string targetId, DirectoryInfo[] dirs)
+    {
+        if (dirs != null)
+        {
+            foreach (DirectoryInfo dir in dirs)
+            {
+                if (dir.Name.Contains(".framework"))
+                {
+                    var output = string.Format("file {0}/Frameworks/{1}/{2}", pathToBuiltProject, dir.Name, dir.Name.Split('.')[0]).Bash();
+                    if (output.Contains("dynamically"))
+                    {
+                        PBXProjectExtensions.AddFileToEmbedFrameworks(proj, targetId, proj.FindFileGuidByProjectPath("Frameworks/" + dir.Name));
+                    }
+                }
+                else
+                {
+                    AddDynamicFrameworkToEmbed(pathToBuiltProject, proj, targetId, new DirectoryInfo(dir.FullName).GetDirectories());
+                }
+            }
+        }
+    }
+#endif
+}
---
+++ Assets/Editor/easy/ShellHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace easy
+{
+    public static class ShellHelper
+    {
+        public static string Bash(this string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = String.Format("-c \"{0}\"", escapedArgs),
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            return result;
+        }
+    }
+}
+
